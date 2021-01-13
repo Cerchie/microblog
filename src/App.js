57d7e1c9db97fe6c8809bf1b8c37156d10a8254a
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-13 14:32:32 -0700

    adds prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Redirect } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+import NavBar from './NavBar'
+import Home from './Home'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <BrowserRouter>
+                    <NavBar />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route exact path="/new">
+                            <NewPost />
+                        </Route>
+                        <Route exact path="/post/:post_id">
+                            <PostView />
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
