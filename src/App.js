41d5a90c254053ef4928586acbd8a58c5344f47c
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-14 08:23:57 -0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import { BrowserRouter, Redirect } from 'react-router-dom'
-import { Route, Switch } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Switch } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import NavBar from './NavBar'
 import Home from './Home'
+import PostView from './PostView'
+import NewEditPost from './NewEditPost'
+
 import './App.css'
 
+import { v4 as uuid } from 'uuid'
+
 function App() {
+    const INITIAL_STATE = [
+        {
+            id: uuid(),
+            title: 'sample post',
+            description: 'just a sample',
+            body: 'sample sample sample sample',
+        },
+    ]
+    const [posts, setPosts] = useState(INITIAL_STATE)
+    const addPost = (newPost) => {
+        setPosts((posts) => [...posts, { ...newPost, id: uuid() }])
+        window.history.pushState(newPost, 'new_post')
+    }
     return (
         <div className="App">
             <header className="App-header">
                 <BrowserRouter>
-                    <NavBar />
                     <Switch>
-                        <Route exact path="/">
-                            <Home />
+                        <NavBar />
+                        <Route exact path="/" component={Home}>
+                            <Home posts={posts} />
                         </Route>
                         <Route exact path="/new">
-                            <NewPost />
+                            <NewEditPost addPost={addPost} />
                         </Route>
                         <Route exact path="/post/:post_id">
                             <PostView />
